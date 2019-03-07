21a22df42d4aa32ab2a98098b491f7a9e7bdf570
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-03-07 21:16:12 +0000

    move test to the root directory. rename the test class to be more descriptive. added a more complex test.

---
+++ test/deployment/src/test/java/MavenApplicationTest.java
@@ -0,0 +1,60 @@
+import graql.lang.Graql;
+import graql.lang.query.GraqlGet;
+import graql.lang.query.GraqlQuery;
+import org.junit.Test;
+
+import static graql.lang.Graql.and;
+import static graql.lang.Graql.match;
+import static graql.lang.Graql.or;
+import static graql.lang.Graql.rel;
+import static graql.lang.Graql.var;
+import static org.junit.Assert.assertEquals;
+
+public class MavenApplicationTest {
+    private void assertQueryEquals(GraqlQuery expected, GraqlQuery parsed, String query) {
+        assertEquals(expected, parsed);
+        assertEquals(expected, Graql.parse(parsed.toString()));
+        assertEquals(query, expected.toString());
+    }
+
+    @Test
+    public void testRelationQuery() {
+        String query = "match\n" +
+                "$brando 'Marl B' isa name;\n" +
+                "(actor: $brando, $char, production-with-cast: $prod);\n" +
+                "get $char, $prod;";
+        GraqlGet parsed = Graql.parse(query).asGet();
+
+        GraqlGet expected = match(
+                var("brando").val("Marl B").isa("name"),
+                rel("actor", "brando").rel("char").rel("production-with-cast", "prod")
+        ).get("char", "prod");
+
+        assertQueryEquals(expected, parsed, query.replace("'", "\""));
+    }
+
+    @Test
+    public void testPredicateQuery1() {
+        String query = "match\n" +
+                "$x isa movie, has title $t;\n" +
+                "{ $t 'Apocalypse Now'; } or { $t < 'Juno'; $t > 'Godfather'; } or { $t 'Spy'; };\n" +
+                "$t !== 'Apocalypse Now';\n" +
+                "get;";
+        GraqlGet parsed = Graql.parse(query).asGet();
+
+        GraqlGet expected = match(
+                var("x").isa("movie").has("title", var("t")),
+                or(
+                        var("t").val("Apocalypse Now"),
+                        and(
+                                var("t").lt("Juno"),
+                                var("t").gt("Godfather")
+                        ),
+                        var("t").val("Spy")
+                ),
+                var("t").neq("Apocalypse Now")
+        ).get();
+
+        assertQueryEquals(expected, parsed, query.replace("'", "\""));
+    }
+}
