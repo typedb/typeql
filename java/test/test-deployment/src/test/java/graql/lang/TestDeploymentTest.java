0a9fd0567a70f96e50a111022860c03a2bf01a56
Ganeshwara Herawan Hananda <ganeshwara@grakn.ai>
2019-03-07 16:51:08 +0000

    refactor CI job, add comments, add TODOs

--- java/test/test-deployment/src/test/java/graql/lang/TestDeploymentTest.java
+++ java/test/test-deployment/src/test/java/graql/lang/TestDeploymentTest.java
@@ -4,12 +4,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
+// TODO: think of a better class name
 public class TestDeploymentTest {
+    // TODO: more tests
     @Test
-    public void test() {
+    public void queryBuilder_shouldConvertFromAndToStringCorrectly() {
         final String query = "match $t sub thing; get;";
         assertEquals(query, Graql.parse(query).toString());
     }
