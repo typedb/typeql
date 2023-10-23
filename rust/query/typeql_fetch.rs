fd31b633ef5a1b7764282757b3d55603bb795872
joshua <joshua@vaticle.com>
2023-10-23 22:25:00 +0100

    WIP implementing fetch data structure

--- rust/query/typeql_fetch.rs
+++ rust/query/typeql_fetch.rs
@@ -19,14 +19,18 @@
  * under the License.
  */
 use std::fmt;
+use std::path::Display;
+use itertools::Itertools;
 
-use crate::common::error::collect_err;
 use crate::common::{Result, token};
+use crate::common::error::collect_err;
 use crate::common::validatable::Validatable;
-use crate::pattern::VariablesRetrieved;
-use crate::query::MatchClause;
+use crate::pattern::{Label, VariablesRetrieved};
+use crate::query::{MatchClause, TypeQLGetAggregate};
 use crate::query::modifier::Modifiers;
+use crate::variable::Variable;
 use crate::variable::variable::VariableRef;
+use crate::write_joined;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct TypeQLFetch {
@@ -36,8 +40,15 @@ pub struct TypeQLFetch {
 }
 
 impl TypeQLFetch {
-    pub(crate) fn new(clause_match: MatchClause) -> Self {
-        TypeQLFetch { clause_match, modifiers: Modifiers::default() }
+    fn validate_names_are_unique(&self) -> Result {
+        // TODO: either do restrict no-reuse across sub-trees or fully allow re-use
+        todo!()
+    }
+}
+
+impl TypeQLFetch {
+    pub(crate) fn new(clause_match: MatchClause, projections: Vec<Projection>) -> Self {
+        TypeQLFetch { clause_match, projections, modifiers: Modifiers::default() }
     }
 }
 
@@ -59,11 +70,104 @@ impl VariablesRetrieved for TypeQLFetch {
     }
 }
 
+#[derive(Debug, Eq, PartialEq)]
+enum Projection {
+    Variable(ProjectionKey),
+    Attribute(ProjectionKey, Vec<ProjectionAttribute>),
+    Subquery(ProjectionLabel, ProjectionSubquery),
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct ProjectionKey {
+    variable: Variable,
+    label: Option<ProjectionLabel>
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum ProjectionLabel {
+    Quoted(String),
+    Unquoted(String),
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct ProjectionAttribute {
+    attribute: Label,
+    label: Option<ProjectionLabel>,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum ProjectionSubquery {
+    GetAggregate(TypeQLGetAggregate),
+    Fetch(Box<TypeQLFetch>)
+}
+
 impl fmt::Display for TypeQLFetch {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "{}", self.clause_match)?;
-        writeln!(f, "{}", token::Clause::Fetch)
-        // write_joined!(f, ";\n", self.statements, self.rules)?;
-        // f.write_str(";")
+        writeln!(f, "{}", token::Clause::Fetch)?;
+        write_joined!(f, ";\n", self.projections)?;
+        writeln!(f, "{}", self.modifiers)
+    }
+}
+
+impl fmt::Display for Projection {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Projection::Variable(key) => {
+                writeln!(f, "{};", key)
+            }
+            Projection::Attribute(key, attrs) => {
+                write!(f, "{}: ", key)?;
+                // TODO fix
+                writeln!(f, " {};", attrs.iter().map(Display::fmt)).join(", ")
+            }
+            Projection::Subquery(label, subquery) => {
+                writeln!(f, "{}: {{\n {} }};", label, subquery)
+            }
+        }
+    }
+}
+
+impl fmt::Display for ProjectionKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.variable)?;
+        if let Some(label) = &self.label {
+            write!(f, " {}", label)
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl fmt::Display for ProjectionLabel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ProjectionLabel::Quoted(s) => write!(f, "\"{}\"", s),
+            ProjectionLabel::Unquoted(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+impl fmt::Display for ProjectionAttribute {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.attribute)?;
+        if let Some(label) = &self.label {
+            write!(f, " {}", label)
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl fmt::Display for ProjectionSubquery {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ProjectionSubquery::GetAggregate(query) => {
+                write!(f, "{}", query)
+            }
+            ProjectionSubquery::Fetch(query) => {
+                write!(f, "{}", query)
+            }
+        }
     }
 }
