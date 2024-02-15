bf830772903e71f7aaec84dd9a4ea6114e111536
Joshua Send <joshua@vaticle.com>
2024-02-15 09:47:21 +0000

    Allow variables to have a leading digit (#317)
    ## Usage and product changes
    
    We modify the behaviour of #310 which unified variables and labels to
    have the same valid identifier syntax, which eliminated the capability
    of variables to have a leading number. For example, the variable `$0`
    was banned.
    
    This PR reverts this specific behaviour, and enables usage of variables
    with leading digits:
    ```
    match
    $1_a isa entity;
    get;
    ```
    is made valid again.
    
    Testing specified in
    https://github.com/vaticle/typedb-behaviour/pull/281
    
    ## Implementation
    
    We split the identifier regexes and grammar rules that validated labels
    and variables before, to be specific to `Variable` and `Label`
    identifiers.

--- rust/common/identifier.rs
+++ rust/common/identifier.rs
@@ -23,10 +23,7 @@ use std::sync::OnceLock;
 
 use regex::{Regex, RegexBuilder};
 
-pub fn is_valid_identifier(identifier: &str) -> bool {
-    static REGEX: OnceLock<Regex> = OnceLock::new();
-    let regex = REGEX.get_or_init(|| {
-        let identifier_start = "A-Za-z\
+const IDENTIFIER_CHAR: &str = "A-Za-z\
             \\u00C0-\\u00D6\
             \\u00D8-\\u00F6\
             \\u00F8-\\u02FF\
@@ -38,17 +35,29 @@ pub fn is_valid_identifier(identifier: &str) -> bool {
             \\u3001-\\uD7FF\
             \\uF900-\\uFDCF\
             \\uFDF0-\\uFFFD";
-        let identifier_tail = format!(
-            "{}\
-            0-9\
-            _\
+const IDENTIFIER_CONNECTOR: &str = "_\
             \\-\
             \\u00B7\
             \\u0300-\\u036F\
-            \\u203F-\\u2040",
-            identifier_start
-        );
-        let identifier_pattern = format!("^[{}][{}]*$", identifier_start, identifier_tail);
+            \\u203F-\\u2040";
+const IDENTIFIER_DIGIT: &str = "0-9";
+
+pub fn is_valid_label_identifier(identifier: &str) -> bool {
+    static REGEX: OnceLock<Regex> = OnceLock::new();
+    let regex = REGEX.get_or_init(|| {
+        let identifier_tail = format!("{}{}{}", IDENTIFIER_CHAR, IDENTIFIER_CONNECTOR, IDENTIFIER_DIGIT);
+        let identifier_pattern = format!("^[{}][{}]*$", IDENTIFIER_CHAR, identifier_tail);
+        RegexBuilder::new(&identifier_pattern).build().unwrap()
+    });
+    regex.is_match(identifier)
+}
+
+pub fn is_valid_var_identifier(identifier: &str) -> bool {
+    static REGEX: OnceLock<Regex> = OnceLock::new();
+    let regex = REGEX.get_or_init(|| {
+        let identifier_head = format!("{}{}", IDENTIFIER_CHAR, IDENTIFIER_DIGIT);
+        let identifier_tail = format!("{}{}{}", IDENTIFIER_CHAR, IDENTIFIER_DIGIT, IDENTIFIER_CONNECTOR);
+        let identifier_pattern = format!("^[{}][{}]*$", identifier_head, identifier_tail);
         RegexBuilder::new(&identifier_pattern).build().unwrap()
     });
     regex.is_match(identifier)
