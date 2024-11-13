f69ef0fc313a247693ff41e0c16a5779bd748934
Haikal Ryclova-Pribadi <haikal@typedb.com>
2024-11-13 14:23:31 +0000

    Renamed //dependencies/vaticle to /typedb

--- java/test/behaviour/debug/DebugTest.java
+++ java/test/behaviour/debug/DebugTest.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         strict = true,
         plugin = "pretty",
-        glue = "com.vaticle.typeql.lang.test.behaviour",
+        glue = "com.typedb.typeql.lang.test.behaviour",
         features = "test/behaviour/debug/debug.feature"
 )
 public class DebugTest {
@@ -29,7 +29,7 @@ public class DebugTest {
     // 4) Ensure 'Target Expression' is set correctly: '//<this>/<package>/<name>:test'
     //
     // 5) Update 'Bazel Flags':
-    //    a) Remove the line that says: '--test_filter=com.vaticle.typeql.lang.*'
+    //    a) Remove the line that says: '--test_filter=com.typedb.typeql.lang.*'
     //    b) Use the following Bazel flags:
     //       --cache_test_results=no : to make sure you're not using cache
     //       --test_output=streamed : to make sure all output is printed
