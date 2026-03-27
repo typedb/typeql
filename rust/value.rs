a730d2bd302c5ee373a842c95d70ae30754d6f9d
Krishnan Govindraj <krishnan@typedb.com>
2026-03-27 21:24:03 +0100

    And replay the brokn stuff so I can fix it

--- rust/value.rs
+++ rust/value.rs
@@ -341,18 +341,7 @@ impl fmt::Display for StructLiteral {
     }
 }
 
-
 impl StringLiteral {
-    fn unescape_unicode<'a>(bytes: &'a [u8]) -> std::result::Result<char, &'a str> {
-        let as_hex = std::str::from_utf8(bytes).expect("Should still be utf8");
-        if bytes.len() == 4 {
-            let as_u32 = u32::from_str_radix(as_hex, 16).map_err(|_| as_hex)?;
-            char::from_u32(as_u32).ok_or(as_hex)
-        } else {
-            Err(as_hex)
-        }
-    }
-
     pub fn unescape(&self) -> Result<String> {
         self.process_unescape(|bytes| {
             if bytes.len() < 2 {
@@ -393,28 +382,31 @@ impl StringLiteral {
         assert!(matches!(bytes[0], b'\'' | b'"'));
 
         let escaped_string = &self.value[1..self.value.len() - 1];
-        let mut buf = String::with_capacity(escaped_string.len());
+        let mut buf = Vec::with_capacity(escaped_string.len());
         let mut rest = escaped_string;
-
         while !rest.is_empty() {
-            let (char, escaped_len) = if rest.as_bytes()[0] == b'\\' {
+            let escaped_len = if rest.as_bytes()[0] == b'\\' {
                 match escape_handler(rest.as_bytes()) {
-                    Ok((char, escaped_len)) => (char, escaped_len),
+                    Ok((char, escaped_len)) => {
+                        let start = buf.len();
+                        buf.resize(buf.len() + char.len_utf8(),0);
+                        char.encode_utf8(&mut buf[start..]);
+                        rest = &rest[escaped_len..];
+                    },
                     Err(considered_escape_byte_length) => {
+                        let considered_escape_sequence = rest.chars().take(considered_escape_byte_length).collect();
                         return Err(TypeQLError::InvalidStringEscape {
                             full_string: escaped_string.to_owned(),
-                            escape: rest.chars().take(considered_escape_byte_length).collect(),
+                            escape: considered_escape_sequence,
                         }.into());
                     }
                 }
             } else {
-                let char = rest.chars().next().expect("string is non-empty");
-                (char, char.len_utf8())
+                buf.push(rest.as_bytes()[0]);
+                rest = &rest[1..];
             };
-            buf.push(char);
-            rest = &rest[escaped_len..];
         }
-        Ok(buf)
+        Ok(String::from_utf8(buf).expect("Expected valid utf8").to_owned())
     }
 }
 
