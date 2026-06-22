b92331d4bcbc3bf06161a81e76086dfb607c4a2b
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2026-06-22 11:44:25 +0100

    add missing BDD steps

--- rust/tests/behaviour/steps.rs
+++ rust/tests/behaviour/steps.rs
@@ -81,6 +81,10 @@ generic_step_impl! {
     #[step(regex = "typeql schema query; fails.*")]
     #[step("get answers of typeql read query")]
     #[step("get answers of typeql write query")]
+    #[step("get answers of typeql read query with given rows")]
+    #[step("get answers of typeql write query with given rows")]
+    #[step(regex = "typeql read query with given rows; fails.*")]
+    #[step(regex = "typeql write query with given rows; fails.*")]
     async fn typeql_query(_: &mut TypeQLWorld, step: &Step) {
         let query_string = get_step_query(step).trim();
         let parsed = match parse_query(query_string) {
@@ -136,5 +140,6 @@ generic_step_impl! {
     #[step("reasoning schema")]
     #[step("reasoning data")]
     #[step("reasoning query")]
+    #[step("query is given rows")]
     async fn do_nothing(_: &mut TypeQLWorld) {}
 }
